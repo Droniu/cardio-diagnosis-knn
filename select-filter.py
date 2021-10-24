8375bc6261fc733f1108ced6d4b811ceb7d8ff0d
Dudeldel <j.dudek@eltrox.pl>
2021-10-24 21:32:55 +0200

    Kod do AVONU - zmiany

--- select-filter.py
+++ select-filter.py
@@ -1,5 +1,7 @@
 import numpy as np
 from  sklearn.feature_selection  import  SelectKBest , chi2 , f_classif
+
+#import pliku do tabel
 file = open("heart.dat")
 all = np.loadtxt(file, delimiter=" ")
 X = np.zeros((len(all),len(all[0])-1),dtype=np.uint8)
@@ -8,10 +10,9 @@ for i in range(len(all)):
     for j in range (len(all[0])-1):
         X[i][j] = all[i][j]
     Y[i] = all[i][len(all[0])-1]
-#X=X.T
-
-
-featureSelection = SelectKBest(chi2, k=13)
+    
+#selekcja
+featureSelection = SelectKBest(f_classif)
 featureSelection.fit(X, Y)
 ranks = featureSelection.get_support(True)
 for i in ranks:

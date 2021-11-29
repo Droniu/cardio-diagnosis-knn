90f55107b954dc0124b2af18a09c3ef54679bce2
Dudeldel <j.dudek@eltrox.pl>
2021-11-29 03:06:43 +0100

    change in X

--- cardio-diagnosis-knn.py
+++ cardio-diagnosis-knn.py
@@ -4,7 +4,9 @@
 
 #%%writefile "select-filter.py"
 import numpy as np
+from sklearn import neighbors
 from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.base import clone
 
 # loading data to tables
 file = open("heart.dat")
@@ -56,37 +58,72 @@ from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 
 metric_list = ["manhattan", "euclidean"]
+neighbors = [3, 7, 11]
 max_features = 6
 
 for top in range(max_features):
     print("\n")
     # select only top features
-    X = X[:, : (top + 1)]
-
+    Xn = X[:, : (top + 1)]
+    scores = np.zeros((6, 2 * 5))
+    i=0
     for p in metric_list:
-        for k in range(5, 10, 2):
+        for k in neighbors:
             model = KNeighborsClassifier(n_neighbors=k, metric=p)
-            scores = []
-            for train_index, test_index in rskf.split(X, y):
-                X_train, X_test = X[train_index], X[test_index]
+            
+            for fold_id, (train_index, test_index) in enumerate(rskf.split(Xn, y)):
+                #model = clone(model)
+                X_train, X_test = Xn[train_index], Xn[test_index]
                 y_train, y_test = y[train_index], y[test_index]
                 model.fit(X_train, y_train)
                 predict = model.predict(X_test)
-                scores.append(accuracy_score(y_test, predict))
+                scores[i,fold_id] = accuracy_score(y_test, predict)
+            
+            mean_score = mean(scores[i])
+            std_score = std(scores[i])
+            print('Accuracy(k=%d, %s metric, top %d features): %.3f (%.3f)' % (k, p, top+1, mean_score, std_score))
+            i = i+1
 
-            mean_score = mean(scores)
-            std_score = std(scores)
-            print(
-                "Accuracy(k=%d, %s metric, top %d features): %.3f (%.3f)"
-                % (k, p, top + 1, mean_score, std_score)
-            )
+            
+    from tabulate import tabulate
+    from scipy.stats import ttest_rel
 
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
+    alfa = 0.07
+    t_statistic = np.zeros((len(metric_list)*len(neighbors), len(metric_list)*len(neighbors)))
+    p_value = np.zeros((len(metric_list)*len(neighbors), len(metric_list)*len(neighbors)))
 
-clfs = {
-    "GNB": GaussianNB(),
-    "kNN": KNeighborsClassifier(),
-    "CART": DecisionTreeClassifier(random_state=42),
-}
+    for i in range(6):
+        for j in range(6):
+            t_statistic[i, j], p_value[i, j] = ttest_rel(scores[i], scores[j])
+    headers = ["manhattan5", "manhattan7", "manhattan9", "euclidean5", "euclidean7", "euclidean9"]
+    names_column = np.array([["manhattan5"], ["manhattan7"], ["manhattan9"], ["euclidean5"], ["euclidean7"], ["euclidean9"]])   
+
+    t_statistic_table = np.concatenate((names_column, t_statistic), axis=1)
+    t_statistic_table = tabulate(t_statistic_table, headers, floatfmt=".2f")
+    p_value_table = np.concatenate((names_column, p_value), axis=1)
+    p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
+    #print("For " + str(top + 1) + " features")
+    #print("t-statistic:\n", t_statistic_table, "\n\np-value:\n", p_value_table)
+
+    advantage = np.zeros((6, 6))
+    advantage[t_statistic > 0] = 1
+    advantage_table = tabulate(np.concatenate(
+        (names_column, advantage), axis=1), headers)
+    #print("For " + str(top + 1) + " features")
+    #print("Advantage:\n", advantage_table)
+    # p_value sprawdzenie które różnice są statystycznie znaczące
+    significance = np.zeros((6,6))
+    significance[p_value <= alfa] = 1
+    significance_table = tabulate(np.concatenate(
+        (names_column, significance), axis=1), headers)
+    #print("For " + str(top + 1) + " features")
+    #print("Statistical significance (alpha = 0.05):\n", significance_table)
+    
+    # połączenie powyższych tabel
+    stat_better = significance * advantage
+    stat_better_table = tabulate(
+        np.concatenate((names_column, stat_better), axis=1), headers
+    )
+    #print("For " + str(top + 1) + " features")
+    #print("Statistically significantly better :\n", stat_better_table)
+
--- stat_analysis.py
+++ stat_analysis.py
@@ -1,23 +1,21 @@
-import numpy as np
-from scipy.stats import ttest_rel
-from sklearn.base import clone
-from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
-from sklearn.feature_selection import SelectKBest, f_classif
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.tree import DecisionTreeClassifier
-from tabulate import tabulate
+# Jakub Dudek
+# Michał Droń
+# knn - dataset statlog heart
 
-# Pobranie danych i zapisanie ich jako X i Y tą lepszą metodą
+#%%writefile "select-filter.py"
+import numpy as np
+from sklearn import neighbors
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.base import clone
+
+# loading data to tables
 file = open("heart.dat")
 all = np.loadtxt(file, delimiter=" ")
 X = all[:, :-1]
 y = all[:, -1].astype(int)
-
 features_quantity = len(X[0] - 1)
+
+# feature selection
 featureSelection = SelectKBest(f_classif)
 featureSelection.fit(X, y)
 
@@ -48,60 +46,79 @@ permutation = complement(feature_order)
 idx = np.empty_like(permutation)
 idx[permutation] = np.arange(len(permutation))
 X = X[:, idx]
+print(X)
 
-# Deklaracja klasyfikatorów
-classifiers = {
-    "GNB": GaussianNB(),
-    "kNN": KNeighborsClassifier(),
-    "CART": DecisionTreeClassifier(random_state=42),
-    "MLP": MLPClassifier(alpha=1, max_iter=1000),
-    "RFC": RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    "ADA": AdaBoostClassifier(),
-}
+from sklearn.model_selection import RepeatedStratifiedKFold
+
+rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=5, random_state=10)
 
 
-# 5 powtórzeniowa, dwukrotna walidacja krzyżowa
-num_of_splits = 5
-num_of_repeats = 2
-rskf = RepeatedStratifiedKFold(
-    n_splits=num_of_splits, n_repeats=num_of_repeats, random_state=42
-)
+from numpy import mean, std
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+
+metric_list = ["manhattan", "euclidean"]
+neighbors = [3, 7, 11]
 max_features = 6
 
 for top in range(max_features):
-    # deklaracja narazie pustej tablicy wyników klasyfikacji
-    scores = np.zeros((len(classifiers), num_of_splits * num_of_repeats))
-    X = X[:, : (top + 1)]
-    # pętla dla każdego "złożenia"
-    for fold_id, (train, test) in enumerate(rskf.split(X, y)):
-        # Dla każdego klasyfikatora
-        for classifier_id, classifier_name in enumerate(classifiers):
-            classifier = clone(classifiers[classifier_name])  # kopia obiektu
-            classifier.fit(X[train], y[train])
-            prediction = classifier.predict(X[test])
-            scores[classifier_id, fold_id] = accuracy_score(y[test], prediction)
-            mean = np.mean(scores, axis=1)
-            std = np.std(scores, axis=1)
+    print("\n")
+    # select only top features
+    X = X[:, : (top + 6)]
+    scores = np.zeros((6, 2 * 5))
+    i=0
+    for p in metric_list:
+        for k in neighbors:
+            model = KNeighborsClassifier(n_neighbors=k, metric=p)
+            
+            for fold_id, (train_index, test_index) in enumerate(rskf.split(X, y)):
+                model = clone(model)
+                X_train, X_test = X[train_index], X[test_index]
+                y_train, y_test = y[train_index], y[test_index]
+                model.fit(X_train, y_train)
+                predict = model.predict(X_test)
+                scores[i,fold_id] = accuracy_score(y_test, predict)
+            
+            mean_score = mean(scores[i])
+            std_score = std(scores[i])
+            #print('Accuracy(k=%d, %s metric, top %d features): %.3f (%.3f)' % (k, p, top+1, mean_score, std_score))
+            i = i+1
 
-    alfa = 0.05
-    t_statistic = np.zeros((len(classifiers), len(classifiers)))
-    p_value = np.zeros((len(classifiers), len(classifiers)))
+            
+    from tabulate import tabulate
+    from scipy.stats import ttest_rel
 
-    for i in range(len(classifiers)):
-        for j in range(len(classifiers)):
+    alfa = 0.07
+    t_statistic = np.zeros((len(metric_list)*len(neighbors), len(metric_list)*len(neighbors)))
+    p_value = np.zeros((len(metric_list)*len(neighbors), len(metric_list)*len(neighbors)))
+
+    for i in range(6):
+        for j in range(6):
             t_statistic[i, j], p_value[i, j] = ttest_rel(scores[i], scores[j])
+    headers = ["manhattan5", "manhattan7", "manhattan9", "euclidean5", "euclidean7", "euclidean9"]
+    names_column = np.array([["manhattan5"], ["manhattan7"], ["manhattan9"], ["euclidean5"], ["euclidean7"], ["euclidean9"]])   
 
-    headers = ["GNB", "kNN", "CART", "MLP", "RFC", "ADA"]
-    names_column = np.array([["GNB"], ["kNN"], ["CART"], ["MLP"], ["RFC"], ["ADA"]])
+    t_statistic_table = np.concatenate((names_column, t_statistic), axis=1)
+    t_statistic_table = tabulate(t_statistic_table, headers, floatfmt=".2f")
+    p_value_table = np.concatenate((names_column, p_value), axis=1)
+    p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
+    #print("For " + str(top + 1) + " features")
+    #print("t-statistic:\n", t_statistic_table, "\n\np-value:\n", p_value_table)
 
-    # T_statistics zaznaczenie wartości > 0 (czyli który lepszy od którego)
-    advantage = np.zeros((len(classifiers), len(classifiers)))
+    advantage = np.zeros((6, 6))
     advantage[t_statistic > 0] = 1
-
+    advantage_table = tabulate(np.concatenate(
+        (names_column, advantage), axis=1), headers)
+    #print("For " + str(top + 1) + " features")
+    #print("Advantage:\n", advantage_table)
     # p_value sprawdzenie które różnice są statystycznie znaczące
-    significance = np.zeros((len(classifiers), len(classifiers)))
+    significance = np.zeros((6,6))
     significance[p_value <= alfa] = 1
-
+    significance_table = tabulate(np.concatenate(
+        (names_column, significance), axis=1), headers)
+    #print("For " + str(top + 1) + " features")
+    #print("Statistical significance (alpha = 0.05):\n", significance_table)
+    
     # połączenie powyższych tabel
     stat_better = significance * advantage
     stat_better_table = tabulate(
@@ -109,3 +126,4 @@ for top in range(max_features):
     )
     print("For " + str(top + 1) + " features")
     print("Statistically significantly better :\n", stat_better_table)
+
